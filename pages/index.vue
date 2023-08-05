1eceeb86f7ff6f72a47cf8802e5e94e1eb568730
Tony Costanzo <tonycos92@gmail.com>
2023-08-04 19:16:11 -0500

    chore: replace local image with CDN

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="md:flex gap-4 block items-center">
-		<div class="md:w-1/2">
-			<NuxtImg src="https://api.takeoffhelper.com/storage/v1/object/public/tech-hive/hero.jpg" class="md:rounded-xl md:rounded-l-none"/>
+	<div class="md:flex gap-4 block items-center mt-8 md:mt-16 xl:mt-32">
+		<div class="md:w-3/5">
+			<img alt="hero image" :src="heroImg" class="md:rounded-xl md:rounded-l-none"/>
 		</div>
-		<div class="w-full mt-8 md:mt-0 md:w-1/2 text-center md:text-right pr-0 md:pr-6 lg:pr-16 xl:pr-20 2xl:pr-24 text-white">
+		<div class="w-full mt-8 md:mt-0 md:w-2/5 text-center md:text-right pr-0 md:pr-6 lg:pr-16 xl:pr-20 2xl:pr-24 text-white">
 			<p class="text-4xl md:text-6xl lg:text-7xl 2xl:text-9xl font-teko">
 				TechHive Labs
 			</p>
@@ -13,10 +13,19 @@
 		</div>
 	</div>
 </template>
-<script>
-import heroImg from "assets/hero.jpg";
+<script setup>
 
 definePageMeta({
 	layout: "main",
+});
+
+const heroImg = ref('')
+const supabase = useSupabaseClient()
+onMounted(() => {
+	heroImg.value = supabase.storage.from("tech-hive").getPublicUrl("hero.jpg", {
+		width: 700,
+		height: 700,
+		format: "webp",
+	}).data.publicUrl;
 });
 </script>
