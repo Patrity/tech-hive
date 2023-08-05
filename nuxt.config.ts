fc736c3c56bfeef2af922f63c7d82c7ba9bbc5aa
Tony Costanzo <tonycos92@gmail.com>
2023-08-04 20:16:04 -0500

    chore: disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-    timeline: {
-      enabled: true
-    }
-  },
+    runtimeConfig: {
+        public: {
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://techhivelabs.com',
+            siteName: 'Awesome Site',
+            siteDescription: 'Welcome to my awesome site!',
+            language: 'en', // prefer more explicit language codes like `en-AU` over `en`
+        }
+    },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/devtools',
     '@nuxt/image',
     '@pinia/nuxt',
     '@nuxtjs/supabase'
+  ],
+  extends: [
+    'nuxt-seo-kit'
   ]
 })
