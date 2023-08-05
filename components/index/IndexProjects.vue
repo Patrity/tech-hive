158b46b9b6e83d993b999c317c352845bf19ff20
Tony Costanzo <tonycos92@gmail.com>
2023-08-05 16:09:31 -0500

    feat: index projects start

---
+++ components/index/IndexProjects.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="mt-24 mb-24 px-4 md:px-0">
+		<div class="w-full md:w-10/12 lg:w-8/12 md:mx-auto">
+			<p class="font-teko text-white text-3xl md:text-5xl lg:text-6xl 2xl:text-8xl text-center">
+				OUR PROJECTS
+			</p>
+			<p class="mt-6">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam orci, tincidunt at risus sed,
+				vestibulum feugiat erat. Quisque pulvinar tempus nisi sit amet consequat. Maecenas euismod nibh ac dolor
+				porta, sed ultrices nulla tempor. Phasellus eget vestibulum nunc, at aliquet metus. In lacinia, lectus
+				sed elementum consectetur, justo ipsum scelerisque dui, nec gravida neque est et nisi. Vivamus faucibus
+				aliquet ligula, sed congue libero mollis ac. Sed aliquet ornare massa, non mollis metus hendrerit sit
+				amet. Cras posuere at sem at cursus. Donec sed porta lacus. Proin auctor tortor lobortis arcu rhoncus
+				hendrerit. Nulla vel sem in quam convallis cursus. Sed auctor et ante quis commodo. Proin aliquet tellus
+				et aliquet facilisis.
+			</p>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
 	<IndexHero />
 	<IndexWhoWeAre />
+	<IndexProjects />
 </template>
 <script setup lang="ts">
 
 import IndexHero from "~/components/index/IndexHero.vue";
+import IndexProjects from "~/components/index/IndexProjects.vue";
 
 definePageMeta({
 	layout: "main",
