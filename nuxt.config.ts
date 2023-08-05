1c8cbf99b2f807605a4134b78a6d198a132bdf1e
Tony Costanzo <tonycos92@gmail.com>
2023-08-04 21:07:49 -0500

    chore: impl seo kit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://techhivelabs.com',
-            siteName: 'Awesome Site',
-            siteDescription: 'Welcome to my awesome site!',
+            siteName: 'TechHive Labs',
+            siteDescription: 'Programming, Marketing, and Consulting',
             language: 'en', // prefer more explicit language codes like `en-AU` over `en`
         }
     },
